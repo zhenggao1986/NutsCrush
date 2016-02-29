0bf9b326173e5638029a713b55696f2119c0ad8f
Zheng Gao <zhenggao1986@gmail.com>
2016-02-28 22:46:16 -0500

    Initial Commit

---
+++ NutsCrush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NutsCrush
+//
+//  Created by Zheng Gao on 2/28/16.
+//  Copyright © 2016 Zheng Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
